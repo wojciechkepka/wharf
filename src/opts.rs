137f3b28cbc6c5bec951643b62ccd2ab28f00775
wojciechkepka <wojtek.kepka@protonmail.com>
2019-12-08 13:24:30 +0100

    start implementing ContainerBuilderOpts

--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 pub trait Query {
     fn to_query(self) -> Vec<(&'static str, String)>;
 }
@@ -135,5 +136,128 @@ impl ContainerLogsOpts {
     /// Only return this number of log lines from the end of the logs. Specify as an integer or all to output all log lines
     pub fn tail(&mut self, tail: String) {
         self.tail = tail;
+    }
+}
+
+#[derive(Default)]
+pub struct ContainerBuilderOpts {
+    Hostname: String,
+    Domainname: String,
+    User: String,
+    AttachStdin: bool,
+    AttachStdout: bool,
+    AttachStderr: bool,
+    ExposedPorts: Value,
+    Tty: bool,
+    OpenStdin: bool,
+    StdinOnce: bool,
+    Env: Vec<String>,
+    Cmd: Vec<String>,
+    Healthcheck: Value,
+    ArgsEscaped: bool,
+    Image: String,
+    Volumes: Value,
+    WorkingDir: String,
+    Entrypoint: Vec<String>,
+    NetworkDisabled: bool,
+    MacAddress: String,
+    OnBuild: Vec<String>,
+    Lables: Value,
+    StopSignal: String,
+    StopTimeout: i64,
+    Shell: Vec<String>,
+    HostConfig: Value,
+    NetworkConfig: Value,
+}
+impl ContainerBuilderOpts {
+    pub fn new() -> Self {
+        ContainerBuilderOpts::default()
+    }
+    /// The hostname to use for the container, as a valid RFC 1123 hostname.
+    pub fn hostname<S: Into<String>>(&mut self, hostname: S) {
+        self.Hostname = hostname.into();
+    }
+    /// The domain name to use for the container.
+    pub fn domain_name<S: Into<String>>(&mut self, domain_name: S) {
+        self.Domainname = domain_name.into();
+    }
+    /// The user that commands are run as inside the container.
+    pub fn User<S: Into<String>>(&mut self, user: S) {
+        self.User = user.into();
+    }
+    /// Whether to attach to stdin.
+    pub fn attach_stdin(&mut self, attach: bool) {
+        self.AttachStdin = attach;
+    }
+    /// Whether to attach to stdout.
+    pub fn attach_stdout(&mut self, attach: bool) {
+        self.AttachStdout = attach;
+    }
+    /// Whether to attach to stderr.
+    pub fn attach_stderr(&mut self, attach: bool) {
+        self.AttachStderr = attach;
+    }
+    /// Attach standard streams to a TTY, including stdin if it is not closed.
+    pub fn tty(&mut self, tty: bool) {
+        self.Tty = tty;
+    }
+    /// Open stdin.
+    pub fn open_stdin(&mut self, open: bool) {
+        self.OpenStdin = open;
+    }
+    /// Close stdin after one attached client disconnects
+    pub fn stdin_once(&mut self, stdin_once: bool) {
+        self.StdinOnce = stdin_once;
+    }
+    /// A list of environment variables to set inside the container in the form ["VAR=value", ...].
+    /// A variable without = is removed from the environment, rather than to have an empty value.
+    pub fn Env(&mut self, env: &[String]) {
+        self.Env = env.to_vec();
+    }
+    /// Command to run specified as a string or an array of strings.
+    pub fn Cmd(&mut self, cmd: &[String]) {
+        self.Cmd = cmd.to_vec();
+    }
+    /// Command is already escaped (Windows only)
+    pub fn args_escaped(&mut self, escaped: bool) {
+        self.ArgsEscaped = escaped;
+    }
+    /// The name of the image to use when creating the container
+    pub fn image<S: Into<String>>(&mut self, image: S) {
+        self.Image = image.into();
+    }
+    /// The working directory for commands to run in.
+    pub fn working_dir<S: Into<String>>(&mut self, dir: S) {
+        self.WorkingDir = dir.into();
+    }
+    /// The entry point for the container as a string or an array of strings.
+    /// If the array consists of exactly one empty string ([""]) then the entry point is reset to system default
+    /// (i.e., the entry point used by docker when there is no ENTRYPOINT instruction in the Dockerfile).
+    pub fn entrypoint(&mut self, entrypoint: &[String]) {
+        self.Entrypoint = entrypoint.to_vec();
+    }
+    /// Disable networking for the container.
+    pub fn network_disabled(&mut self, disabled: bool) {
+        self.NetworkDisabled = disabled;
+    }
+    /// MAC address of the container.
+    pub fn mac_address<S: Into<String>>(&mut self, addr: S) {
+        self.MacAddress = addr.into();
+    }
+    /// ONBUILD metadata that were defined in the image's Dockerfile.
+    pub fn on_build(&mut self, md: &[String]) {
+        self.OnBuild = md.to_vec();
+    }
+    /// Signal to stop a container as a string or unsigned integer.
+    pub fn stop_signal<S: Into<String>>(&mut self, signal: S) {
+        self.StopSignal = signal.into();
+    }
+    /// Timeout to stop a container in seconds.
+    pub fn stop_timeout(&mut self, timeout: i64) {
+        self.StopTimeout = timeout;
+    }
+    /// Shell for when RUN, CMD, and ENTRYPOINT uses a shell.
+    pub fn shell(&mut self, s: &[String]) {
+        self.Shell = s.to_vec();
     }
 }

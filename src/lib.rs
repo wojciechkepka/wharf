73723e8ae2205c028c06fc01fc6f17fb26a0a5c1
wojciechkepka <wojtek.kepka@protonmail.com>
2019-12-08 18:48:58 +0100

    add public api from Docker

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ pub mod containers;
 pub mod images;
 pub mod networks;
 pub mod opts;
+use crate::containers::Containers;
+use crate::images::Images;
+use crate::networks::Networks;
 use failure::Error;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -21,6 +24,18 @@ impl Docker {
             url: Url::parse(s)?,
             client: reqwest::Client::new(),
         })
+    }
+    /// Get reference to api interface of containers
+    pub fn containers(&self) -> Containers {
+        Containers::new(&self)
+    }
+    /// Get reference to api interface of images
+    pub fn images(&self) -> Images {
+        Images::new(&self)
+    }
+    /// Get reference to api interface of networks
+    pub fn networks(&self) -> Networks {
+        Networks::new(&self)
     }
 }
 

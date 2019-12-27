cd4041f359c89820dd8bcab68844a127e0c98ab5
wojciech.kepka <wojtek.kepka@protonmail.com>
2019-12-27 08:25:33 +0100

    add create_a_container example

---
+++ examples/create_a_container.rs
@@ -0,0 +1,29 @@
+use failure::Error;
+use std::collections::HashMap;
+use wharf::{opts::ContainerBuilderOpts, Docker};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    pretty_env_logger::init();
+    // create docker api instance
+    let d = Docker::new("http://0.0.0.0:2376/")?;
+    // get handle to containers
+    let c = d.containers();
+
+    let mut labels = HashMap::new();
+    labels.insert("id", "8927323891274986127");
+
+    // Configure a container
+    let mut opts = ContainerBuilderOpts::new();
+    opts.image("ubuntu:latest")
+        .tty(true)
+        .shell(&["/bin/bash".into()])
+        .cmd(&["/bin/echo", "test"])
+        .labels(&labels)
+        .env(&["HTTP_PROXY=http://proxy.domain.com"]);
+
+    // Create a container
+    c.create("container_name", &opts).await?;
+
+    Ok(())
+}
